66e96125523042808f1d437282c6265afafdc8bb
Nyctef <nyctef@nyctef.com>
2014-04-20 13:30:37 +0000

    parse request timestamps in new base class

--- api/parsedevelinkresult.py
+++ api/parsedevelinkresult.py
@@ -1,8 +1,12 @@
 import evelink.api
 import evelink.eve
 import evelink.char
+from datetime import datetime
 
 class ParsedEvelinkResult(object):
 
     def __init__(self, api_result):
         self.api_result = api_result
+        self.request_time = datetime.fromtimestamp(request.timestamp)
+        self.cache_expires = datetime.fromtimestamp(request.expires)
+
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -10,12 +10,9 @@ unfinishedJobs = APIResult(result={218425036: {'status': 'failed', 'delivered': 
 finishedJobs = APIResult(result={218425036: {'status': 'failed', 'delivered': False, 'runs': 50, 'end_ts': 1397080697, 'installer_id': 94534727, 'begin_ts': 1397068737, 'pause_ts': None, 'container_type_id': 57, 'container_id': 60015036, 'activity_id': 1, 'multipliers': {'char_material': 1.25, 'material': 1.0, 'char_time': 0.920000016689301, 'time': 1.0}, 'line_id': 201762, 'finished': False, 'system_id': 30045305, 'output': {'flag': 4, 'bpc_runs': 0, 'location_id': 60015036, 'container_location_id': 30045305, 'type_id': 222}, 'input': {'blueprint_type': 'copy', 'runs_left': 200, 'mat_level': 5, 'prod_level': 2, 'type_id': 1137, 'item_flag': 4, 'location_id': 60015036, 'id': 1014362750679, 'quantity': 1}, 'install_ts': 1397068737}}, timestamp=1397082331, expires=1397083231)
 deliveredJobs = APIResult(result={218425036: {'status': 'delivered', 'delivered': True, 'runs': 50, 'end_ts': 1397080697, 'installer_id': 94534727, 'begin_ts': 1397068737, 'pause_ts': None, 'container_type_id': 57, 'container_id': 60015036, 'activity_id': 1, 'multipliers': {'char_material': 1.25, 'material': 1.0, 'char_time': 0.920000016689301, 'time': 1.0}, 'line_id': 201762, 'finished': False, 'system_id': 30045305, 'output': {'flag': 4, 'bpc_runs': 0, 'location_id': 60015036, 'container_location_id': 30045305, 'type_id': 222}, 'input': {'blueprint_type': 'copy', 'runs_left': 200, 'mat_level': 5, 'prod_level': 2, 'type_id': 1137, 'item_flag': 4, 'location_id': 60015036, 'id': 1014362750679, 'quantity': 1}, 'install_ts': 1397068737}}, timestamp=1397085582, expires=1397086482)
 
-def from_ts(timestamp):
-    return datetime.fromtimestamp(timestamp)
-
 def clockAtTimeOfRequest(jobsResult):
     clock = Clock()
-    clock.now = Mock(return_value=from_ts(jobsResult.timestamp))
+    clock.now = Mock(return_value=jobsResult.request_time)
     return clock
 
 class ApiTests(unittest.TestCase):
