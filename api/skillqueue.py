1fb904f64e08b66917e2e3560d6180974300f2f0
Nyctef <nyctef@nyctef.com>
2014-04-20 15:58:02 +0000

    fix bad copy-paste

--- api/skillqueue.py
+++ api/skillqueue.py
@@ -6,7 +6,7 @@ from parsedevelinkresult import ParsedEvelinkResult
 class SkillQueue(ParsedEvelinkResult):
 
     def __init__(self, api_result):
-        super(IndustryJobs, self).__init__(api_result)
+        super(SkillQueue, self).__init__(api_result)
         self.is_empty = False
         if len(self.api_result.result) == 0:
             self.is_empty = True
